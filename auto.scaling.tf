0f0f0b6ddcf376aba9b4832427bbf6d0960f3b44
seraaboagye <sera.aboagye@gmail.com>
2020-01-11 13:23:04 -0600

    autoscaling

--- auto.scaling.tf
+++ auto.scaling.tf
@@ -1,39 +1,39 @@
-resource "aws_autoscaling_attachment" "bastions-acirrustech-com" {
-  elb                    = "${aws_elb.bastion-acirrustech-com.id}"
-  autoscaling_group_name = "${aws_autoscaling_group.bastions-acirrustech-com.id}"
+resource "aws_autoscaling_attachment" "bastions-resolversgroup-com" {
+  elb                    = "${aws_elb.bastion-resolversgroup-com.id}"
+  autoscaling_group_name = "${aws_autoscaling_group.bastions-resolversgroup-com.id}"
 }
 
-resource "aws_autoscaling_attachment" "master-eu-west-1a-masters-acirrustech-com" {
-  elb                    = "${aws_elb.api-acirrustech-com.id}"
-  autoscaling_group_name = "${aws_autoscaling_group.master-eu-west-1a-masters-acirrustech-com.id}"
+resource "aws_autoscaling_attachment" "master-eu-west-1a-masters-resolversgroup-com" {
+  elb                    = "${aws_elb.api-resolversgroup-com.id}"
+  autoscaling_group_name = "${aws_autoscaling_group.master-eu-west-1a-masters-resolversgroup-com.id}"
 }
 
-resource "aws_autoscaling_attachment" "master-eu-west-1b-masters-acirrustech-com" {
-  elb                    = "${aws_elb.api-acirrustech-com.id}"
-  autoscaling_group_name = "${aws_autoscaling_group.master-eu-west-1b-masters-acirrustech-com.id}"
+resource "aws_autoscaling_attachment" "master-eu-west-1b-masters-resolversgroup-com" {
+  elb                    = "${aws_elb.api-resolversgroup-com.id}"
+  autoscaling_group_name = "${aws_autoscaling_group.master-eu-west-1b-masters-resolversgroup-com.id}"
 }
 
-resource "aws_autoscaling_attachment" "master-eu-west-1c-masters-acirrustech-com" {
-  elb                    = "${aws_elb.api-acirrustech-com.id}"
-  autoscaling_group_name = "${aws_autoscaling_group.master-eu-west-1c-masters-acirrustech-com.id}"
+resource "aws_autoscaling_attachment" "master-eu-west-1c-masters-resolversgroup-com" {
+  elb                    = "${aws_elb.api-resolversgroup-com.id}"
+  autoscaling_group_name = "${aws_autoscaling_group.master-eu-west-1c-masters-resolversgroup-com.id}"
 }
 
-resource "aws_autoscaling_group" "bastions-acirrustech-com" {
-  name                 = "bastions.acirrustech.com"
-  launch_configuration = "${aws_launch_configuration.bastions-acirrustech-com.id}"
+resource "aws_autoscaling_group" "bastions-resolversgroup-com" {
+  name                 = "bastions.resolversgroup.com"
+  launch_configuration = "${aws_launch_configuration.bastions-resolversgroup-com.id}"
   max_size             = 1
   min_size             = 1
-  vpc_zone_identifier  = ["${aws_subnet.utility-eu-west-1a-acirrustech-com.id}", "${aws_subnet.utility-eu-west-1b-acirrustech-com.id}", "${aws_subnet.utility-eu-west-1c-acirrustech-com.id}"]
+  vpc_zone_identifier  = ["${aws_subnet.utility-eu-west-1a-resolversgroup-com.id}", "${aws_subnet.utility-eu-west-1b-resolversgroup-com.id}", "${aws_subnet.utility-eu-west-1c-resolversgroup-com.id}"]
 
   tag = {
     key                 = "KubernetesCluster"
-    value               = "acirrustech.com"
+    value               = "resolversgroup.com"
     propagate_at_launch = true
   }
 
   tag = {
     key                 = "Name"
-    value               = "bastions.acirrustech.com"
+    value               = "bastions.resolversgroup.com"
     propagate_at_launch = true
   }
 
@@ -59,22 +59,22 @@ resource "aws_autoscaling_group" "bastions-acirrustech-com" {
   enabled_metrics     = ["GroupDesiredCapacity", "GroupInServiceInstances", "GroupMaxSize", "GroupMinSize", "GroupPendingInstances", "GroupStandbyInstances", "GroupTerminatingInstances", "GroupTotalInstances"]
 }
 
-resource "aws_autoscaling_group" "master-eu-west-1a-masters-acirrustech-com" {
-  name                 = "master-eu-west-1a.masters.acirrustech.com"
-  launch_configuration = "${aws_launch_configuration.master-eu-west-1a-masters-acirrustech-com.id}"
+resource "aws_autoscaling_group" "master-eu-west-1a-masters-resolversgroup-com" {
+  name                 = "master-eu-west-1a.masters.resolversgroup.com"
+  launch_configuration = "${aws_launch_configuration.master-eu-west-1a-masters-resolversgroup-com.id}"
   max_size             = 1
   min_size             = 1
-  vpc_zone_identifier  = ["${aws_subnet.eu-west-1a-acirrustech-com.id}"]
+  vpc_zone_identifier  = ["${aws_subnet.eu-west-1a-resolversgroup-com.id}"]
 
   tag = {
     key                 = "KubernetesCluster"
-    value               = "acirrustech.com"
+    value               = "resolversgroup.com"
     propagate_at_launch = true
   }
 
   tag = {
     key                 = "Name"
-    value               = "master-eu-west-1a.masters.acirrustech.com"
+    value               = "master-eu-west-1a.masters.resolversgroup.com"
     propagate_at_launch = true
   }
 
@@ -100,22 +100,22 @@ resource "aws_autoscaling_group" "master-eu-west-1a-masters-acirrustech-com" {
   enabled_metrics     = ["GroupDesiredCapacity", "GroupInServiceInstances", "GroupMaxSize", "GroupMinSize", "GroupPendingInstances", "GroupStandbyInstances", "GroupTerminatingInstances", "GroupTotalInstances"]
 }
 
-resource "aws_autoscaling_group" "master-eu-west-1b-masters-acirrustech-com" {
-  name                 = "master-eu-west-1b.masters.acirrustech.com"
-  launch_configuration = "${aws_launch_configuration.master-eu-west-1b-masters-acirrustech-com.id}"
+resource "aws_autoscaling_group" "master-eu-west-1b-masters-resolversgroup-com" {
+  name                 = "master-eu-west-1b.masters.resolversgroup.com"
+  launch_configuration = "${aws_launch_configuration.master-eu-west-1b-masters-resolversgroup-com.id}"
   max_size             = 1
   min_size             = 1
-  vpc_zone_identifier  = ["${aws_subnet.eu-west-1b-acirrustech-com.id}"]
+  vpc_zone_identifier  = ["${aws_subnet.eu-west-1b-resolversgroup-com.id}"]
 
   tag = {
     key                 = "KubernetesCluster"
-    value               = "acirrustech.com"
+    value               = "resolversgroup.com"
     propagate_at_launch = true
   }
 
   tag = {
     key                 = "Name"
-    value               = "master-eu-west-1b.masters.acirrustech.com"
+    value               = "master-eu-west-1b.masters.resolversgroup.com"
     propagate_at_launch = true
   }
 
@@ -141,22 +141,22 @@ resource "aws_autoscaling_group" "master-eu-west-1b-masters-acirrustech-com" {
   enabled_metrics     = ["GroupDesiredCapacity", "GroupInServiceInstances", "GroupMaxSize", "GroupMinSize", "GroupPendingInstances", "GroupStandbyInstances", "GroupTerminatingInstances", "GroupTotalInstances"]
 }
 
-resource "aws_autoscaling_group" "master-eu-west-1c-masters-acirrustech-com" {
-  name                 = "master-eu-west-1c.masters.acirrustech.com"
-  launch_configuration = "${aws_launch_configuration.master-eu-west-1c-masters-acirrustech-com.id}"
+resource "aws_autoscaling_group" "master-eu-west-1c-masters-resolversgroup-com" {
+  name                 = "master-eu-west-1c.masters.resolversgroup.com"
+  launch_configuration = "${aws_launch_configuration.master-eu-west-1c-masters-resolversgroup-com.id}"
   max_size             = 1
   min_size             = 1
-  vpc_zone_identifier  = ["${aws_subnet.eu-west-1c-acirrustech-com.id}"]
+  vpc_zone_identifier  = ["${aws_subnet.eu-west-1c-resolversgroup-com.id}"]
 
   tag = {
     key                 = "KubernetesCluster"
-    value               = "acirrustech.com"
+    value               = "resolversgroup.com"
     propagate_at_launch = true
   }
 
   tag = {
     key                 = "Name"
-    value               = "master-eu-west-1c.masters.acirrustech.com"
+    value               = "master-eu-west-1c.masters.resolversgroup.com"
     propagate_at_launch = true
   }
 
@@ -182,22 +182,22 @@ resource "aws_autoscaling_group" "master-eu-west-1c-masters-acirrustech-com" {
   enabled_metrics     = ["GroupDesiredCapacity", "GroupInServiceInstances", "GroupMaxSize", "GroupMinSize", "GroupPendingInstances", "GroupStandbyInstances", "GroupTerminatingInstances", "GroupTotalInstances"]
 }
 
-resource "aws_autoscaling_group" "nodes-acirrustech-com" {
-  name                 = "nodes.acirrustech.com"
-  launch_configuration = "${aws_launch_configuration.nodes-acirrustech-com.id}"
+resource "aws_autoscaling_group" "nodes-resolversgroup-com" {
+  name                 = "nodes.resolversgroup.com"
+  launch_configuration = "${aws_launch_configuration.nodes-resolversgroup-com.id}"
   max_size             = 3
   min_size             = 3
-  vpc_zone_identifier  = ["${aws_subnet.eu-west-1a-acirrustech-com.id}", "${aws_subnet.eu-west-1b-acirrustech-com.id}", "${aws_subnet.eu-west-1c-acirrustech-com.id}"]
+  vpc_zone_identifier  = ["${aws_subnet.eu-west-1a-resolversgroup-com.id}", "${aws_subnet.eu-west-1b-resolversgroup-com.id}", "${aws_subnet.eu-west-1c-resolversgroup-com.id}"]
 
   tag = {
     key                 = "KubernetesCluster"
-    value               = "acirrustech.com"
+    value               = "resolversgroup.com"
     propagate_at_launch = true
   }
 
   tag = {
     key                 = "Name"
-    value               = "nodes.acirrustech.com"
+    value               = "nodes.resolversgroup.com"
     propagate_at_launch = true
   }
 
@@ -222,3 +222,4 @@ resource "aws_autoscaling_group" "nodes-acirrustech-com" {
   metrics_granularity = "1Minute"
   enabled_metrics     = ["GroupDesiredCapacity", "GroupInServiceInstances", "GroupMaxSize", "GroupMinSize", "GroupPendingInstances", "GroupStandbyInstances", "GroupTerminatingInstances", "GroupTotalInstances"]
 }
+
